47d24f12e277b8778d2d8324c77f108911ff00e6
Hongyuan Zhang <66273343+Alias-z@users.noreply.github.com>
2024-01-14 03:26:11 +0100

    Update web_demo.py

--- web_demo.py
+++ web_demo.py
@@ -75,10 +75,10 @@ def main():
     model, tokenizer = load_model()
     print("load model end.")
 
-    user_avator = "doc/imgs/user.png"
-    robot_avator = "doc/imgs/robot.png"
+    user_avator = "images/user.png"
+    robot_avator = "images/robot.png"
 
-    st.title("InternLM-Chat-7B")
+    st.title("InternLM-Chat-7B Alias Personal Assitant")
 
     generation_config = prepare_generation_config()
 
